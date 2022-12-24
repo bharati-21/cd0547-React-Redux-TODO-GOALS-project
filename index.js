66252717b14aac2b0278819ea9a8fb6e806e473d
Bharati Subramanian <bharatisharada@gmail.com>
2022-12-24 23:42:39 +0530

    action creators

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // library code
 
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+// action creators
+const addTodoAction = (todo) => {
+	return {
+		type: ADD_TODO,
+		todo,
+	};
+};
+
+const removeTodoAction = (id) => {
+	return {
+		type: REMOVE_TODO,
+		id,
+	};
+};
+
+const toggleTodoAction = (id) => {
+	return {
+		type: TOGGLE_TODO,
+		id,
+	};
+};
+
+const addGoalAction = (goal) => {
+	return {
+		type: ADD_GOAL,
+		goal,
+	};
+};
+
+const removeGoalAction = (goal) => {
+	return {
+		type: REMOVE_GOAL,
+		id,
+	};
+};
+
 function createStore(reducer) {
 	// The store has 4 parts:
 	// 1. State
@@ -38,11 +80,11 @@ function createStore(reducer) {
 // reducer
 function todos(state = [], action) {
 	switch (action.type) {
-		case "ADD_TODO":
+		case ADD_TODO:
 			return state.concat([action.todo]);
-		case "REMOVE_TODO":
+		case REMOVE_TODO:
 			return state.filter((item) => item.id !== action.id);
-		case "TOGGLE_TODO":
+		case TOGGLE_TODO:
 			return action.map((item) =>
 				item.id === action.id
 					? { ...item, complete: !item.complete }
@@ -55,9 +97,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
 	switch (action.type) {
-		case "ADD_GOAL":
+		case ADD_GOAL:
 			return state.concat([action.goal]);
-		case "REMOVE_GOAL":
+		case REMOVE_GOAL:
 			return state.filter((item) => item.id !== action.id);
 		default:
 			return state;
@@ -76,14 +118,14 @@ const store = createStore(app);
 store.subscribe(() => {
 	console.log("New state is ", store.getState());
 });
-store.dispatch({
-	type: "ADD_TODO",
-	todo: {
+
+store.dispatch(
+	addTodoAction({
 		id: 0,
 		name: "Learn redux",
 		complete: false,
-	},
-});
+	})
+);
 // const unsubscribe = store.subscribe(() => {
 // 	console.log("The new state is: " + store.getState());
 // });
