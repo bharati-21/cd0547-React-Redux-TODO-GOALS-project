ed26a8b921652be743c2b37d6af0fd473dfe2493
Bharati Subramanian <bharatisharada@gmail.com>
2022-12-24 23:30:33 +0530

    feat: update using reducer and dispatch

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function createStore() {
+// library code
+
+function createStore(reducer) {
 	// The store has 4 parts:
 	// 1. State
 	// 2. Get the state
@@ -9,21 +11,61 @@ function createStore() {
 	let listeners = [];
 
 	const getState = () => state;
+
 	const subscribe = (listener) => {
 		listeners.push(listener);
 
+		// return the funtion to unsubscribe the passed listener/ function
 		return () => {
 			listeners = listeners.filter((l) => l !== listener);
 		};
 	};
+
+	const dispatch = (action) => {
+		state = reducer(state, action);
+
+		listeners.forEach((l) => l());
+	};
+
 	return {
 		getState,
 		subscribe,
+		dispatch,
 	};
 }
 
-const store = createStore();
-const unsubscribe = store.subscribe(() => {
-	console.log("The new state is: " + store.getState());
+// app code
+// reducer
+function todos(state = [], action) {
+	switch (action.type) {
+		case "ADD_TODO":
+			return state.concat([action.todo]);
+		case "REMOVE_TODO":
+			return state.filter((item) => item.id !== action.id);
+		case "TOGGLE_TODO":
+			return action.map((item) =>
+				item.id === action.id
+					? { ...item, complete: !item.complete }
+					: item
+			);
+		default:
+			return state;
+	}
+}
+
+const store = createStore(todos);
+store.subscribe(() => {
+	console.log("New state is ", store.getState());
 });
-unsubscribe();
+store.dispatch({
+	type: "ADD_TODO",
+	todo: {
+		id: 0,
+		name: "Learn redux",
+		complete: false,
+	},
+});
+// const unsubscribe = store.subscribe(() => {
+// 	console.log("The new state is: " + store.getState());
+// });
+// unsubscribe();
